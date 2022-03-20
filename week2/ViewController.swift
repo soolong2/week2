c3880bf14127eab088bc19ecd2969c878740b626
soo <gustn9360@naver.com>
2022-03-20 22:11:58 +0900

    Initial Commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by so on 2022/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
