8bf331ef501d3422b1d7b7e7c6e2b7bb43d65a08
soo <gustn9360@naver.com>
2022-03-21 00:00:52 +0900

    포켓몬테이블구성

---
+++ week2/PoketmonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PoketmonTableViewCell.swift
+//  week2
+//
+//  Created by so on 2022/03/20.
+//
+
+import UIKit
+
+class PoketmonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var poketmonImgeVeiw: UIImageView!
+    
+    @IBOutlet weak var poketmonName: UILabel!
+    
+    @IBOutlet weak var poketmonNum: UILabel!
+    
+    @IBOutlet weak var poketmonIndex: UILabel!
+    
+    
+    
+    
+}
--- week2/ViewController.swift
+++ week2/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 155
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0{
+            var cell = tableView.dequeueReusableCell(withIdentifier: "Poketmongo", for: indexPath) as! PoketmonTableViewCell
+            
+            cell.poketmonNum.text = "1"
+            cell.poketmonName.text = "피카츄"
+            cell.poketmonIndex.text = "전기타입"
+            return cell
+        }else if indexPath.row == 1 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "Poketmongo", for: indexPath) as! PoketmonTableViewCell
+            
+            cell.poketmonNum.text = "2"
+            cell.poketmonName.text = "꼬북이"
+            cell.poketmonIndex.text = "물타입"
+            return cell
+        } else{
+            var cell = tableView.dequeueReusableCell(withIdentifier: "Poketmongo", for: indexPath) as! PoketmonTableViewCell
+            
+            cell.poketmonNum.text = "-"
+            cell.poketmonName.text = "---"
+            cell.poketmonIndex.text = "--타입"
+            return cell
+        }
+    }
 }
+
+
 
